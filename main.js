2226c72cef2fb75a7061546467d6c4e0eacd8da8
nguyenkhachieu <nguyenkhachieu0508@gmail.com>
2020-05-03 14:23:10 +0700

    create directive and model two way binding data

--- main.js
+++ main.js
@@ -2,5 +2,31 @@ var vueInstance = new Vue({
     el: "#app",
     data: {
         title: "kaka",
-    }
+        url: 'https://zingnews.vn/',
+        price: 10000,
+        count: 0,
+        clientX: 0,
+        clientY: 0,
+        message: 'hello',
+        firstName: '',
+        email: '',
+    },
+    methods: {
+        clickCount() {
+            this.count += 1;
+        },
+        handleMouseMove(e) {
+            this.clientX = e.clientX;
+            this.clientY = e.clientY;
+        },
+        handleSubmit(e) {
+            e.preventDefault();
+            console.log(e)
+        },
+    },
+    computed: { // xu ly nhung logic o template vd the div .....
+        reversedMessage() {
+            return this.message.split('').reverse().join('-');
+        }
+    },
 })
