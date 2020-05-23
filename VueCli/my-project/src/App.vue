190b516992ca59109f21e8af32857a90b346b0d2
nguyenkhachieu <nguyenkhachieu0508@gmail.com>
2020-05-23 16:59:11 +0700

    create vue cli

---
+++ VueCli/my-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+    <CompHeader />
+    <CompFooter />
+  </div>
+</template>
+
+<script>
+import CompHeader from './components/CompHeader.vue';
+import CompFooter from './components/CompFooter.vue';
+export default {
+  name: 'App',
+  components: {
+    CompHeader,
+    CompFooter
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VueCli/my-project/src/components/CompFooter.vue
@@ -0,0 +1,14 @@
+<template>
+    <header><h1>{{ text }}</h1></header>
+</template>
+
+<script>
+    export default {
+        name: 'comp-header',
+        data() {
+            return {
+                text: 'Fotter'
+            }
+        }
+    }
+</script>>
